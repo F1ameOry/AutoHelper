6d05c310a5e84a939293b52e1a23469db20bf302
F1ameOry <olegivanov144@mail.ru>
2024-06-26 21:43:33 +0500

    dist add

---
+++ assets/CAppBar.vue_vue_type_script_setup_true_lang-DXe9aZ3P.js
@@ -0,0 +1,1 @@
+import{d as s,c as a,a as e,t as n,o}from"./index-bwdW3Dce.js";const r={class:"h-16 mb-2"},c={class:"fixed w-full top-0 left-0 right-0 h-16 flex flex-row justify-center items-center text-white"},_=s({__name:"CAppBar",props:{screenName:{type:String,default:null}},setup(t){return(l,i)=>(o(),a("div",r,[e("div",c,[e("span",null,n(t.screenName),1)])]))}});export{_};
